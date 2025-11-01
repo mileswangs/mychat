7f08514e69f8ce3f56a1527fc68fd6416e1abe2c
Shuo Wang <ws18022@outlook.com>
2025-11-01 19:54:23 +0800

    save

--- speedrun.sh
+++ speedrun.sh
@@ -1,18 +1,43 @@
+#!/bin/bash
+export OMP_NUM_THREADS=1
 export MYCHAT_BASE_DIR="$HOME/.cache/mychat"
 mkdir -p $MYCHAT_BASE_DIR
 
+# ---------------------------------------------------------
+#python environment and uv 
 
-# #download the eval bundle to evaluate Core metric when training
-# EVAL_BUNDLE_URL=https://karpathy-public.s3.us-west-2.amazonaws.com/eval_bundle.zip
-# if [ ! -d "$MYCHAT_BASE_DIR/eval_bundle" ]; then
-#     curl -L -o eval_bundle.zip $EVAL_BUNDLE_URL
-#     unzip -q eval_bundle.zip
-#     rm eval_bundle.zip
-#     mv eval_bundle $MYCHAT_BASE_DIR
-# fi
+#intall uv (if not installed)
+command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
+# create a .venv local virtual environment (if it doesn't exist)
+[ -d ".venv" ] || uv venv
+# install the repo dependencies
+uv sync --extra gpu
+# activate venv so that `python` uses the project's venv instead of system python
+source .venv/bin/activate
 
-# #download identify conversations to impart a personality to the model
-# if [ ! -f "$MYCHAT_BASE_DIR/identity_conversations.jsonl" ]; then
-#     echo "Downloading identity conversations..."
-#     curl -L -o "$MYCHAT_BASE_DIR/identity_conversations.jsonl" https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
-# fi
+# ---------------------------------------------------------
+# Wandb setup
+# If you wish to use wandb for logging (it's nice!, recommended).
+# 1) Make sure to first log in to wandb, e.g. run:
+#    `wandb login`
+# 2) Set the WANDB_RUN environment variable when running this script, e.g.:
+#    `WANDB_RUN=d26 bash speedrun.sh`
+if [ -z "$WANDB_RUN" ]; then
+    # by default use "dummy" : it's handled as a special case, skips logging to wandb
+    WANDB_RUN=dummy
+fi
+
+#download the eval bundle to evaluate Core metric when training
+EVAL_BUNDLE_URL=https://karpathy-public.s3.us-west-2.amazonaws.com/eval_bundle.zip
+if [ ! -d "$MYCHAT_BASE_DIR/eval_bundle" ]; then
+    curl -L -o eval_bundle.zip $EVAL_BUNDLE_URL
+    unzip -q eval_bundle.zip
+    rm eval_bundle.zip
+    mv eval_bundle $MYCHAT_BASE_DIR
+fi
+
+#download identify conversations to impart a personality to the model
+if [ ! -f "$MYCHAT_BASE_DIR/identity_conversations.jsonl" ]; then
+    echo "Downloading identity conversations..."
+    curl -L -o "$MYCHAT_BASE_DIR/identity_conversations.jsonl" https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
+fi
