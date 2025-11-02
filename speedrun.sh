065ca6047c83a4a51857a63c9907c334386804e6
Shuo Wang <ws18022@outlook.com>
2025-11-02 14:51:20 +0800

    1

--- speedrun.sh
+++ speedrun.sh
@@ -81,21 +81,21 @@ wait $DATASET_DOWNLOAD_PID
 # # evaluate the model on a larger chunk of train/val data and draw some samples
 # torchrun --standalone --nproc_per_node=8 -m scripts.base_loss
 # # evaluate the model on CORE tasks
-torchrun --standalone --nproc_per_node=8 -m scripts.base_eval
+# torchrun --standalone --nproc_per_node=8 -m scripts.base_eval
 
 # # ------------------------------------------------------------------------------------
 # # Midtrain(tech the model conversation special tokens, tool use, multiple during training (~162mb)
 
-# #download identify conversations to impart a personality to the model
-# #see dev/gen_synthetic_data.py for details on how this data was prepared and to get a sense of how you can easily tune it
-# if [ ! -f "$MYCHAT_BASE_DIR/identity_conversations.jsonl" ]; then
-#     echo "Downloading identity conversations..."
-#     curl -L -o "$MYCHAT_BASE_DIR/identity_conversations.jsonl" https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
-# fi
+#download identify conversations to impart a personality to the model
+#see dev/gen_synthetic_data.py for details on how this data was prepared and to get a sense of how you can easily tune it
+if [ ! -f "$MYCHAT_BASE_DIR/identity_conversations.jsonl" ]; then
+    echo "Downloading identity conversations..."
+    curl -L -o "$MYCHAT_BASE_DIR/identity_conversations.jsonl" https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
+fi
 
-# # run midtrain and eval the model
-# torchrun --standalone --nproc_per_node=8 -m scripts.mid_train -- --run=$WANDB_RUN
-# torchrun --standalone --nproc_per_node=8 -m scripts.chat_eval -- -i mid
+# run midtrain and eval the model
+torchrun --standalone --nproc_per_node=8 -m scripts.mid_train -- --run=$WANDB_RUN
+torchrun --standalone --nproc_per_node=8 -m scripts.chat_eval -- -i mid
 
 # # ------------------------------------------------------------------------------------
 # # Supervised Fine-Tuning (SFT) (domain adaptation to each sequence all by itself per row)
